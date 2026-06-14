26eb5f9b9c98703a12b269785a50188cc98b6ad1
abdullahmamun1 <hello.abdullahmamun1@gmail.com>
2026-06-14 06:44:31 +0600

    database table created and sample data added

--- QUERY.sql
+++ QUERY.sql
@@ -98,14 +98,3 @@ INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status
 
 
 
--- Query-1
-SELECT match_id, fixture, base_ticket_price FROM Matches
-WHERE tournament_category = 'Champions League' AND match_status = 'Available';
-
--- Query-2
-SELECT user_id, full_name, email FROM Users
-WHERE full_name ILIKE 'Tanvir%' OR full_name ILIKE '%Haque%'
-  
--- Query-3
-SELECT user_id, full_name, email FROM Users
-WHERE full_name ILIKE 'Tanvir%' OR full_name ILIKE '%Haque%'
